500892692d6bd75e0e4c5d8841359f4d91298269
Amir <vibelessyoung@gmail.com>
2026-02-02 19:05:31 +0330

    create a basic design

--- front/src/components/FileUpload.jsx
+++ front/src/components/FileUpload.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function FileUpload() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>آپلود عکس</h1>
+      <form>
+        <label htmlFor=""></label>
+        <input type="file" name="" id="" />
+        {/* */}
+        <button type="submit" value="send"></button>
+      </form>
+      <div></div>
+    </div>
+  );
 }
 
 export default FileUpload;
